cc59368dccb6593efce4de4a74411db254d39723
Mikołaj Kadłubowski <mikolaj_kadlubowski@wp.pl>
2026-04-30 10:57:20 +0200

    Downloading data

--- downloader.py
+++ downloader.py
@@ -1,8 +1,11 @@
 import yfinance as yf
 
+# Ticker lists
 tick_list = ["PKN.WA", "PKO.WA", "PEO.WA", "KGH.WA", "PZU.WA", "LPP.WA", "ALE.WA", "SPL.WA",
               "CDR.WA", "DNP.WA", "MBK.WA", "PCO.WA", "ALR.WA", "TPE.WA", "PGE.WA", "BDX.WA"]
 
+# Downloading data
 df = yf.download(tick_list, period = "3y")
 
+# Saving data to .csv
 df.to_csv("dane.csv")
