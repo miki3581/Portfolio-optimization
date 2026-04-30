3f0656cd95180732ab93e3eef26640f3b00fe28e
Mikołaj Kadłubowski <mikolaj_kadlubowski@wp.pl>
2026-04-30 11:05:57 +0200

    Downloading data

--- downloader.py
+++ downloader.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 # Ticker lists
 tick_list = ["PKN.WA", "PKO.WA", "PEO.WA", "KGH.WA", "PZU.WA", "LPP.WA", "ALE.WA", "SPL.WA",
-              "CDR.WA", "DNP.WA", "MBK.WA", "PCO.WA", "ALR.WA", "TPE.WA", "PGE.WA", "BDX.WA"]
+              "CDR.WA", "DNP.WA", "MBK.WA", "ALR.WA", "TPE.WA", "PGE.WA", "BDX.WA"]
 
 # Downloading data
 df = yf.download(tick_list, period = "3y")
